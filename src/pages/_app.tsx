8521344f5e34b5ed53c0cbd6d8779f0dbc269909
Eduardo-Ferraz <eduardoc.ferraz2@gmail.com>
2023-01-09 20:08:14 -0300

    Tema e fonte adicionados

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+
+const theme = extendTheme({
+  fonts: {
+    body: 'Josefin Sans, sans-serif',
+    heading: 'Josefin Sans, sans-serif',
+    mono: 'Josefin Sans, sans-serif',
+  },
+  colors: {
+    highlight: "#FFBA08",
+    highlight50: "rgba(255, 186, 8, 0.5)", // Como podemos botar o 50% em HEX ???
+    dark: {
+      black: "#000000",
+      headtext: "#47585B",
+      info: "#999999",
+      info50: "rgba(153, 153, 153, 0.5)", // Como podemos botar o 50% em HEX ???
+    },
+    light: {
+      white: "#FFFFFF",
+      headtext: "#F5F8FA",
+      info: "#DADADA",
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
