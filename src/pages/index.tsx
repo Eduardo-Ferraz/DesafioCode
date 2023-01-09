c9b8cee8c9cb59a0d2f0486081b7ad4b116f4abb
alekswheeler <alexskntks@outlook.com>
2023-01-09 17:27:40 -0300

    add: global css config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <Flex h="100vh" justifyContent="center" alignItems="center">
-      <Text>Este é o template de React para CT Junior, criado em 2022</Text>
+      <p>Este é o template de React para CT Junior, criado em 2022</p>
     </Flex>
   );
 };
